5c22d88d7fbbfc1b9663bd85146b5c9c6dac8ee9
atgunasekar0504-devops <atgunasekar0504@gmail.com>
2026-09-20 17:47:20 +0530

    Create variables.tf

---
+++ modules/dynamodb-module/variables.tf
@@ -0,0 +1,14 @@
+variable "table_name" {
+  description = "DynamoDB table name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
