21c6d447319ab5e2d23bc6c137c84b40afcab9d8
atgunasekar0504-devops <atgunasekar0504@gmail.com>
2026-09-20 18:23:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,7 @@
-variable "aws_region" {
-  description = "AWS region for TBDP-BI resources"
-  type        = string
-}
+AWS_REGION = "ap-southeast-2"
 
-variable "ec2_instance_name" {
-  description = "Name of the EC2 instance"
-  type        = string
-}
+ec2_instance_name =   "tbdp-bi-dev-ec2"
+ec2_instance_type =  "t3.micro"
+ec2_ami_id         =  "ami-0eeab0e1473986ffd"
 
-variable "ec2_instance_type" {
-  description = "EC2 instance type"
-  type        = string
-}
-
-variable "ec2_ami_id" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-}
-
-variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB employee table"
-  type        = string
-}
+dynamodb_table_name = "tbdp-bi-dev-employee-table"
